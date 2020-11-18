ece86c188d0da6ea6839e558ad6a75fda8a0029d
Andrew <andrew11ds@hotmail.es>
2020-11-17 20:16:34 -0500

    Padding variable de la barra principal

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -10,9 +10,18 @@ export class MainComponent implements OnInit {
 
   constructor(private dataService: DataService) {
     this.dataService.getData().subscribe(val => console.log(val));
+      //document.getElementById("yeison").style.padding = "padding-top:100px;";
   }
 
   ngOnInit(): void {
+  }
+
+  get padding(): any{
+    var y=screen.height;
+    var finalpad= Math.ceil((y*250/768))+'px';
+    return{
+      'padding-top':finalpad
+    };
   }
 
 }
