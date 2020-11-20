8d75fc6ff29da7c83bb2e61009acb11454f5ba98
JoseDanielFernandez <josedanielfer@gmail.com>
2020-11-20 14:26:06 -0500

    Logo agregado

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -16,12 +16,12 @@ export class MainComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    $('#nameForm').hide();
+
   }
 
   get padding(): any{
     var y=screen.height;
-    var finalpad= Math.ceil((y*250/768))+'px';
+    var finalpad= Math.ceil((y*10/768))+'px';
     return{
       'padding-top':finalpad
     };
@@ -35,55 +35,9 @@ export class MainComponent implements OnInit {
   go2Manage(){
     $('#ManageHTML').show();
     $('#MainHTML').hide();
-    $('#alerta').show();
-    $('#alerta').fadeIn();
-    setTimeout(function() {
-         $("#alerta").fadeOut();
-    },1000);
-    var n1= $('#number1Main').val()+''
-    var n2= $('#number2Main').val()+''
-    var n3 = $('#number3Main').val()+''
-    console.log(n1,n2,n3)
-    if (n1!='') {
-      console.log("entre")
-     $('#number1').val(n1)
-    }
-    if (n2 !='') {
-      $('#number2').val(n2)
-    }
-    if (n3 !='') {
-      $('#number3').val(n3)
-    }
-
-    var test  =$('#cedula').val()+'';
-    if (test=='') {
-      $('#cerrar').hide();
-      $('#perfil').hide();
-    }
-    //  $('#number2').val($('#number2Main').val());
-    //  $('#number3').val($('#number3Main').val());
-
-  }
-
-  go2Manage2(){
-    $('#ManageHTML').show();
-    $('#MainHTML').hide();
-    var input = $('#inputName').val()+'';
-    if (input != '') {
-      $('#nameInputForm').val(input);
-    }
-    var test  =$('#cedula').val()+'';
-    if (test=='') {
-      $('#cerrar').hide();
-      $('#perfil').hide();
-    }
-  }
-
-  switch2Name(){
-    $('#dirForm').hide();
-    $('#nameForm').show();
-    $('#dirRestForm').hide();
-    $('#nameRestForm').show();
+    $('#number1').val($('#number1Main').val().toString());
+    $('#number2').val($('#number2Main').val().toString());
+    $('#number3').val($('#number3Main').val().toString());
   }
 
 }
